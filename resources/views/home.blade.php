adae31593143ce585cc50e57f1246086d440a441
Hasan Tareque <hmtareque@gmail.com>
2019-06-05 20:05:33 +0100

    views updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,21 +4,10 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <passport-clients></passport-clients>
-                    <passport-authorized-clients></passport-authorized-clients>
-                    <passport-personal-access-tokens></passport-personal-access-tokens>
-                </div>
-            </div>
+            <passport-clients></passport-clients>
+            <passport-authorized-clients></passport-authorized-clients>
+            <hr/>
+            <passport-personal-access-tokens></passport-personal-access-tokens>
         </div>
     </div>
 </div>
